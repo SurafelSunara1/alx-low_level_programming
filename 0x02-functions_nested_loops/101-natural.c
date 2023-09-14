8b12891ebdee7931a376631e3a5fdf4a9af6bcdc
Surafel Sunara <surafelsunara1@gmail.com>
2023-09-14 00:04:37 -0700

    commit11

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,4 +1,11 @@
-/**
+/*
+ * File: 101-natural.c
+ * Auth: Surafel Sunara
+ */
+ 
+#include <stdio.h>
+
+ /**
  * main - Lists all the natural numbers below 1024 (excluded)
  *        that are multiples of 3 or 5.
  *
